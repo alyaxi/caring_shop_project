627ae717d45ffaf0367f4f69f452de5490d801be
alyaxi <ali.zaman@quanrio.com>
2022-12-23 14:53:43 +0500

    updated

--- social/server/MiddleWare/authMiddleWare.js
+++ social/server/MiddleWare/authMiddleWare.js
@@ -6,12 +6,12 @@ dotenv.config();
 const secret = process.env.JWT_KEY;
 const authMiddleWare = async(req,res,next) => {
     try{
-        const token = req?.headers?.authorization?.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];
         console.log(token)
         if(token){
-            const decoded = jwt?.verify(token, secret);
+            const decoded = jwt.verify(token, secret);
             console.log(decoded)
-            req.body._id = decoded?.id;
+            req.body._id = decoded.id;
         }
         next();
     }
